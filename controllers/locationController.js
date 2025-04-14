cf4f660e998ede26907c1266dfd116b289fbcb23
AKINOLA AYOBAMI <ayobamiakinola84@gmail.com>
2025-04-14 22:15:03 +0100

    fix: country and city

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,11 +1,24 @@
 const Country = require("../model/Country");
 const City = require("../model/City");
+const { countries } = require("countries-list");
 
 const getAllCountries = async (req, res) => {
-    const countries = await Country.find();
-    if (!countries)
-        return res.status(204).json({ message: "No Country found" });
-    res.json(countries);
+    // const countries = await Country.find();
+    try {
+        const countryList = Object.values(countries).map((c) => ({
+            name: c.name,
+            iso2: c.iso2,
+            iso3: c.iso3,
+            currency: c.currency,
+            capital: c.capital,
+            continent: c.continent
+        }));
+        if (!countryList)
+            return res.status(204).json({ message: "No Country found" });
+        res.json(countryList);
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 const getAllCities = async (req, res) => {
--- middleware/loginLimiter.js
+++ middleware/loginLimiter.js
@@ -14,6 +14,7 @@ const loginLimiter = rateLimit({
 			"errlog.log"
 		);
 		res.status(options.statusCode).send(options.message);
+		next();
 	},
 	standardHeaders: true,
 	legacyHeaders: false,
